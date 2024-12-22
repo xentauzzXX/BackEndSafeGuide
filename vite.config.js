e7efe8221f1980d2d2f110b1a0aaaf141e8b93bd
Cyn <11211027@student.itk.ac.id>
2024-12-23 01:39:23 +0800

    update heroku

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+    build: { manifest: true, },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
